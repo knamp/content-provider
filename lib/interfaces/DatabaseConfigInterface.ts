d37c2ae3a7ccd9e25b562eaf8eaa4f9994ceec6e
Hans Christian Reinl <info@drublic.de>
2018-05-16 16:41:25 +0200

    Upgrade DatabaseConfigInterface to represent more options

--- lib/interfaces/DatabaseConfigInterface.ts
+++ lib/interfaces/DatabaseConfigInterface.ts
@@ -1,8 +1,17 @@
 export default interface DatabaseConfigInterface {
   fromMemory: boolean;
+  active?: boolean;
   database?: string;
   dialect?: string;
   host?: string;
   password?: string;
   username?: string;
+  logging?: (...params) => void;
+  pool?: {
+    max?: number;
+    min?: number;
+    idle?: number;
+  };
+  port?: number;
+  seederStorage?: string;
 }
